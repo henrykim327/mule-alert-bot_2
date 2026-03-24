cd5f6bdcb84e23e21c16b46487cd4533e467c730
henrykim327 <henrykim327@gmail.com>
2026-03-25 01:27:07 +0900

    mule_both.py

---
+++ mule_both.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+WEBHOOK_URL = "여기에 디스코드 웹훅 URL 넣기"
+
+URL = "https://www.mule.co.kr/bbs/market/guitar"
+
+KEYWORDS = {
+    "THR10II": 350000,
+    "THR10": 250000
+}
+
+def get_price(text):
+    numbers = re.findall(r'\d+', text)
+    if numbers:
+        return int(numbers[0]) * 10000
+    return None
+
+def send_discord(title, link, price):
+    data = {
+        "content": f"매물 발견!\n{title}\n가격: {price}원\n{link}"
+    }
+    requests.post(WEBHOOK_URL, json=data)
+
+def check_mule():
+    res = requests.get(URL)
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    posts = soup.select("a.subject")
+
+    for post in posts:
+        title = post.text.strip()
+        link = "https://www.mule.co.kr" + post.get("href")
+
+        for keyword, max_price in KEYWORDS.items():
+            if keyword in title:
+                price = get_price(title)
+                if price and price <= max_price:
+                    send_discord(title, link, price)
+
+check_mule()
